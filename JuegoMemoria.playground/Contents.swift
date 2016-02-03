5a59252b79094b8ae13fdbc46047d642a4295cd5
EfrainGuerrero <Efrain@MacBook-Pro.local>
2016-02-03 08:14:11 -0600

    playground

---
+++ JuegoMemoria.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+// Realizado por Eduardo Efraín Guerrero León
+
+import UIKit
+
+var rango = 0...100
+
+for numero in rango {
+    
+    if numero % 5 == 0 {
+        print ("# \(numero) Bingo!!!")
+    }
+    
+    if numero % 2 == 0 {
+        print ("# \(numero) par!!!")
+    }
+    
+    if numero % 2 == 1 {
+        print ("# \(numero) impar!!!")
+    }
+    
+    if numero >= 30 && numero <= 40 {
+        print ("# \(numero) Viva Swift!!!")
+    }
+}
